efaea0a92037bcd2ef1a91bae7e68037ccfb3766
CJ-Kaiser <cj_kaiser@live.com>
2023-09-18 22:04:02 -0400

    Styled form better. Numbers don't go below 0.

--- src/NewScheduleForm.jsx
+++ src/NewScheduleForm.jsx
@@ -15,9 +15,10 @@ const modeOptions = [
   }
 ];
 
+//TODO: Formik for Validation? Refer to project from Ract Basics.
 export function NewScheduleForm({onSubmit}) {
     const [name, setName] = useState("");
-    const [time, setTime] = useState("");
+    const [duration, setDuration] = useState("");
     const [interval, setInterval] = useState("");
     const [itemCount, setItemCount] = useState("");
 
@@ -26,29 +27,35 @@ export function NewScheduleForm({onSubmit}) {
     function handleSubmit(e) {
       e.preventDefault();
       if(name === "") return;
-      if(time === NaN || time <= 0) return;
+      if(duration === NaN || duration <= 0) return;
 
       onSubmit(name);
 
       setName("");
-      setTime("");
+      setDuration("");
     };
 
     const showDurationInput = () => mode != "durationMissing";
     const showIntervalInput = () => mode != "intervalMissing";
     const showItemCountInput = () => mode != "itemCountMissing";
 
+    function validateNumberMax(value, max, setter) {
+      value = Math.max(max, value);
+      setter(value);
+    }
+
     return (
       <form onSubmit={handleSubmit} className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="title">Schedule Name</label>
+        <label htmlFor="title" className="form-input-h">Schedule Name
           <input
             type="text"
             id="title"
             value={name}
             onChange={e => setName(e.target.value)}
           />
-          <label htmlFor="mode">Mode
+        </label>
+        <div className="form-sep"></div>
+        <label htmlFor="mode" className="form-input-h">Mode
             <select
               id="mode"
               value={mode}
@@ -58,38 +65,37 @@ export function NewScheduleForm({onSubmit}) {
                 <option value={option.value} key={option.value}>{option.text}</option>
               ))}
             </select>
-          </label>
-          {showDurationInput() &&
-            <label htmlFor="time">Duration
-              <input
-                type="number"
-                id="time"
-                value={time}
-                onChange={e=>setTime(e.target.value)}
-              />
-            </label>
-          }
-          {showIntervalInput() &&
-            <label htmlFor="interval">Interval
-              <input
-                type="number"
-                id="interval"
-                value={interval}
-                onChange={e=>setTime(e.target.value)}
-              />
-            </label>
-          }
-          {showItemCountInput() && 
-            <label htmlFor="itemCount">Item Count
+        </label>
+          {showItemCountInput() &&
+            <label htmlFor="itemCount" className="form-input-h">Item Count
               <input
                 type="number"
                 id="itemCount"
                 value={itemCount}
-                onChange={e=>setTime(e.target.value)}
+                onChange={e=>validateNumberMax(e.target.value, 1, setItemCount)}
               />
             </label>
           }
-        </div>
+          {showDurationInput() &&
+            <label htmlFor="time" className="form-input-h">Duration
+              <input
+                type="number"
+                id="time"
+                value={duration}
+                onChange={e=>validateNumberMax(e.target.value, 1, setDuration)}
+              />
+            </label>
+          }
+          {showIntervalInput() &&
+            <label htmlFor="interval" className="form-input-h">Interval
+              <input
+                type="number"
+                id="interval"
+                value={interval}
+                onChange={e=>validateNumberMax(e.target.value, 0, setInterval)}
+              />
+            </label>
+          }
         <button className="btn">Add</button>
       </form>
     );
